4f4b0f831d8b0a34ed9cf6420b93bd5c50a51b98
Marcela Cardoso <79878818+marcelajcn@users.noreply.github.com>
2022-12-15 21:45:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args){
+    int num1 = 5;
+     int num2 = 2;
+     int soma = 0; 
+
+    soma = num1 + num2;
+    System.out.println(soma);
+    
+  }
+}
